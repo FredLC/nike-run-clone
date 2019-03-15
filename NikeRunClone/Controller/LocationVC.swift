bd2052d949325b26819afcb8dc5d179f2c0dda49
fredlc <fredlefevre@outlook.com>
2019-03-15 14:04:49 -0400

    MapView setup

---
+++ NikeRunClone/Controller/LocationVC.swift
@@ -0,0 +1,30 @@
+//
+//  LocationVC.swift
+//  NikeRunClone
+//
+//  Created by Fred Lefevre on 2019-03-15.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+    
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+    
+
+}
