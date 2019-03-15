b344c6be609c4f2ee7cc889c04dfce602ed180a3
fredlc <fredlefevre@outlook.com>
2019-03-15 10:56:20 -0400

    Initial project setup

---
+++ NikeRunClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NikeRunClone
+//
+//  Created by Fred Lefevre on 2019-03-15.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NikeRunClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NikeRunClone
+//
+//  Created by Fred Lefevre on 2019-03-15.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
