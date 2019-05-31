4244b51488ef3fb337f095ab595a1462a67dae4d
fredlc <fredlefevre@outlook.com>
2019-05-31 09:03:11 -0400

    Integrated Realm database to app and created Run model

---
+++ NikeRunClone/Extensions/Run.swift
@@ -0,0 +1,35 @@
+//
+//  Run.swift
+//  NikeRunClone
+//
+//  Created by Fred Lefevre on 2019-05-31.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Run: Object {
+    @objc dynamic public private(set) var id = ""
+    @objc dynamic public private(set) var date = NSDate()
+    @objc dynamic public private(set) var pace = 0.0
+    @objc dynamic public private(set) var distance = 0.0
+    @objc dynamic public private(set) var duration = 0
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    override class func indexedProperties() -> [String] {
+        return ["duration", "date"]
+    }
+    
+    convenience init(pace: Double, duration: Int, distance: Double) {
+        self.init()
+        self.id = UUID().uuidString.lowercased()
+        self.date = NSDate()
+        self.pace = pace
+        self.distance = distance
+        self.duration = duration
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NikeRunClone' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NikeRunClone
+  pod 'RealmSwift'
+
+end
