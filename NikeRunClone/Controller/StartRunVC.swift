e5bdf47a3779113a3694fdfc0c8d6c7160b58fa6
fredlc <fredlefevre@outlook.com>
2019-06-02 18:52:47 -0400

    Created LastRun view

--- NikeRunClone/Controller/StartRunVC.swift
+++ NikeRunClone/Controller/StartRunVC.swift
@@ -11,8 +11,13 @@ import MapKit
 
 class StartRunVC: LocationVC {
     
-    
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lastRunView: UIView!
+    @IBOutlet weak var lastRunStackView: UIStackView!
+    @IBOutlet weak var lastRunCloseButton: UIButton!
+    @IBOutlet weak var paceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +28,38 @@ class StartRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
+    }
+    
+    func getLastRun() {
+        guard let lastRun = Run.getRuns()?.first else {
+            lastRunView.isHidden = true
+            lastRunStackView.isHidden = true
+            lastRunCloseButton.isHidden = true
+            return
+        }
+        lastRunView.isHidden = false
+        lastRunStackView.isHidden = false
+        lastRunCloseButton.isHidden = false
+        
+        paceLabel.text = lastRun.pace.formatTimeDurationToString()
+        distanceLabel.text = "\(lastRun.distance.convertMetersToMiles(places: 2))"
+        durationLabel.text = lastRun.duration.formatTimeDurationToString()
     }
 
     @IBAction func centerLocationButtonPressed(_ sender: Any) {
         
     }
     
+    @IBAction func lastRunCloseButtonPressed(_ sender: Any) {
+        lastRunView.isHidden = true
+        lastRunStackView.isHidden = true
+        lastRunCloseButton.isHidden = true
+    }
 }
 
 extension StartRunVC: CLLocationManagerDelegate {
